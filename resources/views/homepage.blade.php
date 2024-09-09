f3dc0f2b5a23a67b3196c88b43e63fceaf37ca5e
alessandrofogliada <fogliada.a@gmail.com>
2024-09-09 14:07:41 +0200

    1.0.0 Helloworld Laravel

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primi passi con laravel</title>
+</head>
+<body>
+    <h1>Helloworld con {{$name_framework}}!!</h1>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name_framework = 'Laravel';
+
+    return view('homepage' , compact('name_framework'));
 });
